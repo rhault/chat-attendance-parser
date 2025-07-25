c5ac0b62d278ce948b18bcbd144d4dc1acc16f38
rhault <raullloredagomez@gmail.com>
2025-07-25 17:24:59 -0300

    extract hour

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-
-import os
 import re
 import xlsxwriter
 from datetime import datetime
-import pandas as pd
 from collections import defaultdict
 
 # Regular expression pattern to parse each line
-LINER_PATTERN = re.compile(r"\[(\d{2}/\d{2}), (\d{2}:\d{2})\] (.*?): (.*)")
+LINER_PATTERN = re.compile(r"(\d{2}/\d{2}/\d{4}) (\d{2}:\d{2}) - (.*?): (.*)")
 
 MESSAGE_HOUR_PATTERN = re.compile(r"\b(\d{2}:\d{2})\b")
+
+
+def extract_hour(text):
+    match = MESSAGE_HOUR_PATTERN.search(text)
+    return match.group(1) if match else ""
 
 
 def read_text_file(file_path):
@@ -18,20 +20,23 @@ def read_text_file(file_path):
     """
     messages = []
 
-    with open(file_path, 'r', encoding='utf-8') as file:
+    with open(file_path, "r", encoding="utf-8") as file:
         for line in file:
             match = LINER_PATTERN.match(line)
             if match:
                 date_str, time_str, sender, message = match.groups()
                 full_datetime = datetime.strptime(
-                    f"{date_str}/2025 {time_str}", "%d/%m/%Y %H:%M")
+                    f"{date_str} {time_str}", "%d/%m/%Y %H:%M"
+                )
 
-                messages.append({
-                    'date': full_datetime.date(),
-                    'time': full_datetime.time(),
-                    'sender': sender.strip(),
-                    'text': message.strip()
-                })
+                messages.append(
+                    {
+                        "date": full_datetime.date(),
+                        "time": full_datetime.time(),
+                        "sender": sender.strip(),
+                        "text": extract_hour(message.strip()),
+                    }
+                )
         return messages
 
 
@@ -42,10 +47,10 @@ def group_messages_data(messages):
     grouped_messages = defaultdict(lambda: defaultdict(list))
 
     for message in messages:
-        sender = message['sender']
-        date = str(message['date'])
-        del message['sender']  # Remove sender from message dict
-        del message['date']  # Remove date from message dict
+        sender = message["sender"]
+        date = str(message["date"])
+        del message["sender"]  # Remove sender from message dict
+        del message["date"]  # Remove date from message dict
         grouped_messages[sender][date].append(message)
 
     return grouped_messages
@@ -61,11 +66,15 @@ def save_to_excel(grouped_data, output_file):
     for sender, dates in grouped_data.items():
         worksheet = workbook.add_worksheet(name=sender[:31])
 
-        worksheet.write(0, 0, 'Data')
-        worksheet.write(0, 1, 'Entrada')
-        worksheet.write(0, 2, 'Saída')
-        worksheet.write(0, 3, 'Entrada')
-        worksheet.write(0, 4, 'Saida')
+        worksheet.write(0, 0, "Data")
+        worksheet.write(0, 1, "Entrada")
+        worksheet.write(0, 2, "Entrada t")
+        worksheet.write(0, 3, "Saida")
+        worksheet.write(0, 4, "Saida t")
+        worksheet.write(0, 5, "Entrada")
+        worksheet.write(0, 6, "Entrada t")
+        worksheet.write(0, 7, "Saida")
+        worksheet.write(0, 8, "Saida t")
 
         row = 1
 
@@ -73,16 +82,18 @@ def save_to_excel(grouped_data, output_file):
             worksheet.write(row, 0, date)
             col = 1
             for input in messages:
-                time = input['time']
+                time = input["time"]
+                text = input["text"]
 
                 worksheet.write(row, col, time.strftime("%H:%M"))
-                col += 1
+                worksheet.write(row, col + 1, text)
+                col += 2
 
             row += 1
 
     workbook.close()
 
 
-read_text = read_text_file('./input/chat.txt')
+read_text = read_text_file("./input/Controle de Jornada.txt")
 group_message = group_messages_data(read_text)
-save_to_excel(group_message, './output/chat_attendance.xlsx')
+save_to_excel(group_message, "./output/chat_attendance.xlsx")
